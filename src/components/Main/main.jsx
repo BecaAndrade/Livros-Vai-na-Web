de062f4e10a1026cfbde5a8f5e9896594ae81c98
BecaAndrade <rebecabarceloz@gmail.com>
2024-11-18 09:20:42 -0300

    Inicio da Header

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -5,7 +5,9 @@ export default function Main(){
 
     return(
         <main className={S.Main}>
+        <div className={S.TextBox}>
         <h1>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h1>
+        </div>
         </main>
     )
 
