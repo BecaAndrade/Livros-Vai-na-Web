84d69c8c7c3f6b6d43469c5443af92227d9cfeca
BecaAndrade <rebecabarceloz@gmail.com>
2025-03-31 15:29:11 -0300

    fiz a parte de pegar todos os livros cadastrados da API

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import S from "./doados.module.scss"
 import Biblioteca from "../../assets/img/bibliotedadameianoite.jpg"
-import Alice from "../../assets/img/Alicenopaisdasmaravilhas.jpg"
-import Coraline from "../../assets/img/Coraline.jpg"
-import HarryPotter from "../../assets/img/HarryPotter.jpg"
-import Clarice from "../../assets/img/UmSoprodeVida.jpg"
-import Hamlet from "../../assets/img/Hamlet.jpg"
-import Dostoievski from "../../assets/img/MemoriasdoSubsolo.jpg"
-import Essencialismo from "../../assets/img/essencialismo.jpg"
+// import Alice from "../../assets/img/Alicenopaisdasmaravilhas.jpg"
+// import Coraline from "../../assets/img/Coraline.jpg"
+// import HarryPotter from "../../assets/img/HarryPotter.jpg"
+// import Clarice from "../../assets/img/UmSoprodeVida.jpg"
+// import Hamlet from "../../assets/img/Hamlet.jpg"
+// import Dostoievski from "../../assets/img/MemoriasdoSubsolo.jpg"
+// import Essencialismo from "../../assets/img/essencialismo.jpg"
+import axios from 'axios';
+import {useState, useEffect} from 'react'
 
 export default function Doados(){
+
+    const [livros, setLivros] = useState([])
+
+    const getLivros = async () => {
+        const response = await axios.get("https://api-livros-ll30.onrender.com/livros")
+        setLivros(response.data)
+    }
+
+    useEffect(()=>{
+        getLivros();
+    },[])
+    
+
     return(
         <>
         <section className={S.Text}>     
@@ -23,8 +38,16 @@ export default function Doados(){
                 <p>Matt Haig</p>
                 <p>Ficção Científica</p>
                 </article>
+                {livros.map((item)=>(
+                <article>
+                 <img src={item.imagem_url} alt="" />
+                 <h3>{item.titulo}</h3>
+                  <p>{item.categoria}</p>
+                  <p>{item.autor}</p>
+                 </article>
+                ))}
             </div>
-            <div>
+            {/* <div>
             <article>
                 <img className={S.livroAlice} src={Alice} alt=" Livro Alice no Pais das Maravilhas" />
                 <h3> Alice no Pais das Maravilhas </h3>
@@ -82,8 +105,8 @@ export default function Doados(){
                 <p> Greg Mckeown </p>
                 <p> Ficção Literária</p>
             </article>
-        
-            </div>
+         */}
+            {/* </div> */}
 
         </section>
 
