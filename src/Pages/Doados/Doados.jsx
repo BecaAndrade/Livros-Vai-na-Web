bb5406dead203a27fb969067337fe1b0c4e3291c
BecaAndrade <rebecabarceloz@gmail.com>
2025-04-03 23:40:32 -0300

    Atualizando o scss

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,13 +1,5 @@
 import React from "react";
 import S from "./doados.module.scss"
-// import Biblioteca from "../../assets/img/bibliotedadameianoite.jpg"
-// import Alice from "../../assets/img/Alicenopaisdasmaravilhas.jpg"
-// import Coraline from "../../assets/img/Coraline.jpg"
-// import HarryPotter from "../../assets/img/HarryPotter.jpg"
-// import Clarice from "../../assets/img/UmSoprodeVida.jpg"
-// import Hamlet from "../../assets/img/Hamlet.jpg"
-// import Dostoievski from "../../assets/img/MemoriasdoSubsolo.jpg"
-// import Essencialismo from "../../assets/img/essencialismo.jpg"
 import axios from 'axios';
 import {useState, useEffect} from 'react'
 
@@ -32,16 +24,11 @@ export default function Doados(){
         </section>
         <section className={S.boxLivros}>
             <div>
-                {/* <article>
-                <img src={Biblioteca} alt="Livro Biblioteca da Meia Noite " />
-                <h3> Biblioteca da Meia Noite </h3>
-                <p>Matt Haig</p>
-                <p>Ficção Científica</p>
-                </article> */}
+            
                 {livros.map((item)=>(
                 <article>
                  <img src={item.imagem_url} alt="" />
-                 <h3>{item.titulo}</h3>
+                 <h2>{item.titulo}</h2>
                   <p>{item.categoria}</p>
                   <p>{item.autor}</p>
                  </article>
